38aa2d92cd7b054c8006f96506625ea1b62fc0e8
Alex-T-T <96976947+Alex-T-T@users.noreply.github.com>
2023-01-28 18:32:01 +0000

    Deploying to gh-pages from @ AlexHorzhij/pet-support-project-frontend@a9192e8b18a852b18e0490503aa9e739be1fa28f 🚀

--- static/js/322.894322cd.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpet_support_project_frontend=self.webpackChunkpet_support_project_frontend||[]).push([[322],{1322:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});n(2791);var r=n(3329);function u(){return(0,r.jsx)("div",{children:"NewsPage"})}}}]);
-//# sourceMappingURL=322.894322cd.chunk.js.map
