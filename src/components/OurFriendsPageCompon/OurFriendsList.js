27e1ddfa2fba00817815d468aa77641eaedbdf49
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2023-01-28 20:55:39 +0200

    added OurFriends page 1st edition

---
+++ src/components/OurFriendsPageCompon/OurFriendsList.js
@@ -0,0 +1,17 @@
+import { Loader } from 'components/Loader/Loader';
+import OurFriendsItem from './OurFriendsItem';
+import css from './OurFriendsList.module.css';
+
+export default function OurFriendsList({ partners }) {
+  if (!partners) {
+    return <Loader />;
+  }
+
+  return (
+    <ul className={css.partnersList}>
+      {partners.map(partner => (
+        <OurFriendsItem key={partner._id} partner={partner} />
+      ))}
+    </ul>
+  );
+}
