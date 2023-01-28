1c839d84ddad2d73a25d9008f1f214719a1bba32
Serhii Kaushnian <skaushnjan@gmail.com>
2023-01-28 12:39:55 +0100

    initial commit

---
+++ src/components/UserPage/UserDataItem.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { Box, IconButton, Typography } from '@mui/material';
+import ModeEditOutlineRoundedIcon from '@mui/icons-material/ModeEditOutlineRounded';
+import DoneRoundedIcon from '@mui/icons-material/DoneRounded';
+
+function UserDataItem({ title = '', value }) {
+  const [inputState, setInputState] = useState(true);
+  const [inputValue, setInputValue] = useState(value);
+  const changeInputState = () => {
+    setInputState(prev => !prev);
+  };
+  const changeInputValue = e => {
+    setInputValue(e.currentTarget.value);
+  };
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '100%',
+        mb: '10px',
+        fontSize: '18px',
+
+        ':last-child': {
+          mb: '0',
+        },
+      }}
+    >
+      <Box sx={{ mr: 'auto' }}>
+        <Typography>{title}:</Typography>
+      </Box>
+      <input
+        style={{
+          borderColor: inputState ? 'transparent' : '#F59256',
+          backgroundColor: inputState ? 'transparent' : '#FDF7F2',
+          borderWidth: '1px',
+          padding: '5px 10px',
+          borderStyle: 'solid',
+          borderRadius: '15px',
+          width: '230px',
+          margin: '0 15px',
+          appearance: 'none',
+          outline: 'none',
+        }}
+        disabled={inputState}
+        value={inputValue}
+        onChange={changeInputValue}
+        onBlur={changeInputState}
+      />
+      <IconButton
+        onClick={changeInputState}
+        sx={{ backgroundColor: '#FDF7F2', width: '32px', height: '32px' }}
+      >
+        {inputState ? (
+          <ModeEditOutlineRoundedIcon
+            sx={{ width: '20px', color: '#F59256' }}
+          />
+        ) : (
+          <DoneRoundedIcon sx={{ width: '25px', color: '#F59256' }} />
+        )}
+      </IconButton>
+    </Box>
+  );
+}
+
+export default UserDataItem;
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import UserData from 'components/UserPage/UserData';
 
-export default function UserPage() {
+function UserPage() {
   return (
-    <div>UserPage</div>
-  )
+    <Container
+      sx={{
+        flexDirection: 'row',
+        display: 'flex',
+        backgroundColor: '#FDF7F2',
+      }}
+    >
+      <Box>
+        <Typography variant="h3" sx={{ ml: '17px' }}>
+          My information:
+        </Typography>
+        <UserData />
+      </Box>
+    </Container>
+  );
 }
+
+export default UserPage;
