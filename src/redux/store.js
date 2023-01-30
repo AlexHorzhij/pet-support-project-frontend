69d4a537118ffa49b8b9c34a3bb701e265072889
OleksandrHorzhij <horzhij@gmail.com>
2023-01-30 20:29:19 +0200

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,9 +11,9 @@ import {
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import { authSlice } from "./auth/authSlice";
-import { noticesSlice } from 'redux/notices/noticesSlice'
-import { newsApi } from "./news/newsSlice";
+import { authSlice } from './auth/authSlice';
+import { noticesSlice } from 'redux/notices/noticesSlice';
+import { newsApi } from './news/newsSlice';
 import { fetchUserDataSlice } from 'redux/userData/userDataSlice';
 import { fetchPetsDataSlice } from 'redux/petsData/petsSlice';
 
@@ -29,19 +29,16 @@ export const store = configureStore({
     notices: noticesSlice.reducer,
     [newsApi.reducerPath]: newsApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => [
-      ...getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }),
-      newsApi.middleware
-  ]
-
-    userData: fetchUserDataSlice.reducer,
-    petsData: fetchPetsDataSlice.reducer,
-  },
- 
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+    newsApi.middleware,
+  ],
+  userData: fetchUserDataSlice.reducer,
+  petsData: fetchPetsDataSlice.reducer,
 });
 
 export const persistor = persistStore(store);
