dd56c0def1e3c8e00e6121165f509ac984d6ddcb
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2023-02-01 20:34:44 +0000

    Deploying to gh-pages from @ AlexHorzhij/pet-support-project-frontend@0c7ee27777c70afdce4fa066cd822eaa8e61bf2a 🚀

--- static/js/605.93cc6be7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpet_support_project_frontend=self.webpackChunkpet_support_project_frontend||[]).push([[605],{7605:function(e,n,r){r.r(n),r.d(n,{default:function(){return u}});r(2791);var s=r(6420),t=r(7904),c=r(3329);function u(){return(0,c.jsxs)(s.o,{children:[(0,c.jsx)(t.G$,{}),(0,c.jsxs)(s.w,{children:[(0,c.jsx)("div",{children:(0,c.jsx)(t.V5,{})}),(0,c.jsx)(s.w,{children:(0,c.jsx)(t.o$,{})})]}),(0,c.jsx)(t.iK,{})]})}}}]);
-//# sourceMappingURL=605.93cc6be7.chunk.js.map
