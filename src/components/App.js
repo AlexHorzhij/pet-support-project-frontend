8c4eb07408b36c2fa3dc23d0d4afe11622a9bb55
OleksandrHorzhij <horzhij@gmail.com>
2023-02-05 19:21:24 +0200

    fix3

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { fetchCurrentUser } from 'redux/auth/authOperations';
 import { Toaster } from 'react-hot-toast';
 import { Routes, Route } from 'react-router-dom';
 import { LoaderPage } from 'components';
-import { PrivateRoute, PublicRoute } from '../services/RouteManager';
+import { PrivateRoute } from '../services/RouteManager';
 
 const SharedLayout = lazy(() => import('./SharedLayout/SharedLayout'));
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
@@ -31,10 +31,10 @@ function App() {
           <Route path="/" element={<SharedLayout />}>
             <Route index element={<HomePage />} />
 
-            <Route element={<PublicRoute />}>
-              <Route path="login" element={<LoginPage />} />
-              <Route path="register" element={<RegisterPage />} />
-            </Route>
+            {/* <Route element={<PublicRoute />}> */}
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterPage />} />
+            {/* </Route> */}
 
             <Route path="news" element={<NewsPage />} />
             <Route path="friends" element={<OurFriendsPage />} />
