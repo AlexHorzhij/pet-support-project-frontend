e1a376233790a08a03a7665aeeb002b67a7bc37c
Oleksandr Tolochko <0504Fusion@gmail.com>
2023-02-05 23:12:58 +0200

    add pagination
    add pagination to newsList

--- src/components/News/NewsList/newsList.js
+++ src/components/News/NewsList/newsList.js
@@ -8,16 +8,32 @@ import { getNews } from 'redux/news/newsSelectors';
 import { fetchNews } from 'redux/news/newsOperations';
 import { sortObjByDate } from 'services/sortObjByDate';
 
+import { useState } from 'react';
+import { Pagination } from '@mui/material';
+import usePagination from '../../../services/pagination';
+
 export const NewsList = () => {
   const dispatch = useDispatch();
-
+ 
   useEffect(() => {
     dispatch(fetchNews());
   }, [dispatch]);
 
   const { news, error, isLoading } = useSelector(getNews);
-
+  
   const sortedNews = sortObjByDate(news, 'date');
+ 
+  // ==============Pagination================
+  const [page, setPage] = useState(1);
+  const PER_PAGE = 6;
+
+  const count = Math.ceil(sortedNews.length / PER_PAGE);
+  const paginationData = usePagination(sortedNews, PER_PAGE)
+  const handleChangePagination = (event, page) => {
+    setPage(page)
+    paginationData.jump(page)    
+  }
+//============================================
 
   return (
     <>
@@ -26,7 +42,7 @@ export const NewsList = () => {
       {news.length === 0 && !isLoading && <NoNewsItem />}
       {news && (
         <NewsGrid component="ul" container columnSpacing={4} rowSpacing={7}>
-          {sortedNews.map(({ _id, title, description, date, url }) => (
+          {paginationData.currentData().map(({ _id, title, description, date, url }) => (
             <NewsItem
               key={_id}
               title={title}
@@ -38,6 +54,16 @@ export const NewsList = () => {
           ))}
         </NewsGrid>
       )}
+      <Pagination
+        color="primary" 
+        count={count}
+        size="large"
+        page={page}
+        variant="outlined"
+        shape="rounded"
+        onChange={handleChangePagination}
+        style={{display: 'flex', justifyContent: 'center'}}
+      />
     </>
   );
 };
