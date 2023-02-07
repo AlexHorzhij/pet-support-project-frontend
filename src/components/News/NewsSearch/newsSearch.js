cc4857ab937e49ef651282fba45e8f9c272d3bdf
Oleksandr Tolochko <0504Fusion@gmail.com>
2023-02-07 18:29:10 +0200

    NewsPage
    newsPage as a parent component to newsList, newsSearch, newsTitle, noNewsItem
    replace all logic to parent component

--- src/components/News/NewsSearch/newsSearch.js
+++ src/components/News/NewsSearch/newsSearch.js
@@ -2,41 +2,9 @@ import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import InputBase from '@mui/material/InputBase';
-import { useDispatch } from 'react-redux';
-// import { useSearchParams } from 'react-router-dom';
-import { toast } from 'react-hot-toast';
-import { useState } from 'react';
 import { FormSearch } from './newsSearch.styled';
-import { fetchSearchNews } from 'redux/news/newsOperations';
 
-
-export default function NewsSearch() {
-  const dispatch = useDispatch();
-  // const [searchParams, setSearchParams] = useSearchParams({});
-  const [text, setText] = useState('')
-
-  // const searchValue = searchParams.get('search')
-
-
-  const handleInputChange = event => {
-    setText(event.currentTarget.value)
-    // setSearchParams( {'search':event.currentTarget.value});
-    };
-
-  const handleInputSubmit = event => {
-    event.preventDefault();
-
-    if (text?.trim() === "" || !text) {
-      toast.error("Please, enter search value!")
-      return
-    }
-    if (text.trim().length < 3 || text.includes('*')) {
-      toast.error("Please, enter no less 3 letters for a correct search!")
-      return
-    }
-
-    dispatch(fetchSearchNews(text));
-  };
+export default function NewsSearch({onChange, onSubmit, value}) {
 
   return (
     <FormSearch
@@ -47,15 +15,15 @@ export default function NewsSearch() {
         alignItems: 'center',
         maxWidth: 400,
       }}
-      onSubmit={handleInputSubmit}
+      onSubmit={onSubmit}
     >
       <InputBase
         type='text'
         sx={{ ml: 1, flex: 1, pl: 1 }}
         placeholder="Search news"
-        inputProps={{ 'aria-label': 'search', 'minlenghts': 3 }}
-        defaultValue = {text}
-        onChange={handleInputChange}
+        inputProps={{ 'aria-label': 'search' }}
+        defaultValue = {value}
+        onChange={onChange}
       />
       <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
         <SearchIcon />
--- src/pages/NewsPage.js
+++ src/pages/NewsPage.js
@@ -4,13 +4,57 @@ import { Title } from 'components/Title/Title';
 import { NewsList } from '../components/News/NewsList/newsList';
 import { Main, NewsContainer } from 'components/News/NewsItem/newsItem.styled';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+
+import { fetchNews, fetchSearchNews } from 'redux/news/newsOperations';
+import { getNews } from 'redux/news/newsSelectors';
+import { sortObjByDate } from 'services/sortObjByDate';
+
 export default function NewsPage() {
+  const [searchParams, setSearchParams] = useSearchParams({});
+  const [value, setValue] = useState('')
+  const searchValue = searchParams.get('search')
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchNews());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchSearchNews(searchValue));
+  }, [dispatch, searchValue]);
+
+  const handleInputChange = event => {
+    setValue(event.currentTarget.value)
+    };
+
+  const handleInputSubmit = event => {
+    event.preventDefault();
+
+    if (value?.trim() === "" ) {
+      toast.error("Please, enter search value!")
+      return
+    }
+    if (value.trim().length < 3|| value.includes('*')) {
+      toast.error("Please, enter no less 3 letters for a correct search!")
+      return
+    }
+    setSearchParams( {'search': value});
+  };
+
+  const { news, error, isLoading } = useSelector(getNews);
+  
+  const sortedNews = sortObjByDate(news, 'date');
+
   return (
     <Main>
       <NewsContainer>
         <Title text="News" />
-        <NewsSearch />
-        <NewsList />
+        <NewsSearch onChange={handleInputChange} onSubmit={handleInputSubmit} value={value} />
+        <NewsList news={sortedNews} isLoading={isLoading} error={error} value={searchValue}/>
       </NewsContainer>
     </Main>
   );
