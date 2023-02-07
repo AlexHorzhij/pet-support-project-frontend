5e2e79c274e9d93c050da5bbc0fb95df4a1fe48d
OleksandrHorzhij <horzhij@gmail.com>
2023-02-07 23:51:04 +0200

    fix routs

--- src/components/NoticesPageComponents/NoticesFilter.js
+++ src/components/NoticesPageComponents/NoticesFilter.js
@@ -4,51 +4,63 @@ import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import { Container, Typography } from '@mui/material';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 
 import { fetchNotices } from 'redux/notices/noticesOperations';
 import { useParams } from 'react-router-dom';
-
+import { getAuth } from 'redux/auth/authSelectors';
 
 export default function NoticesFilter() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const params = useParams()
-  const dispatch = useDispatch()
+  const { token } = useSelector(getAuth);
+  const params = useParams();
+  const dispatch = useDispatch();
 
-  const { categoryName } = params
-  const search = searchParams.get('search') || ''
-
-  const handleFilter = (e) => {
-    e.preventDefault()
-    dispatch(fetchNotices({ categoryName, search }))
-  }
+  const { categoryName } = params;
+  const search = searchParams.get('search') || '';
+  console.log('token filter', token);
+  const handleFilter = e => {
+    e.preventDefault();
+    dispatch(fetchNotices({ categoryName, search }, token));
+  };
 
   return (
-    <Container    >
-      {(<Typography variant='h2'
-        align='center'
-        sx={{ mt: 8, mb: 6, mx: 'auto' }}
-      >
-        Find your favorite pet
-      </Typography>)}
+    <Container>
+      {
+        <Typography
+          variant="h2"
+          align="center"
+          sx={{ mt: 8, mb: 6, mx: 'auto' }}
+        >
+          Find your favorite pet
+        </Typography>
+      }
       <Paper
         component="form"
         sx={{
-          p: '2px 4px', display: 'flex', alignItems: 'center', mb: 6,
-          maxWidth: 608, height: 44, mx: 'auto',
+          p: '2px 4px',
+          display: 'flex',
+          alignItems: 'center',
+          mb: 6,
+          maxWidth: 608,
+          height: 44,
+          mx: 'auto',
         }}
       >
         <InputBase
-          sx={{ ml: 1, flex: 1, pl: 1, }}
+          sx={{ ml: 1, flex: 1, pl: 1 }}
           placeholder="Search"
           inputProps={{ 'aria-label': 'search' }}
           onChange={e => setSearchParams({ search: e.target.value })}
           value={search}
         />
         <IconButton
-          onClick={handleFilter} type="button"
-          sx={{ p: '10px' }} aria-label="search" >
+          onClick={handleFilter}
+          type="button"
+          sx={{ p: '10px' }}
+          aria-label="search"
+        >
           <SearchIcon />
         </IconButton>
       </Paper>
--- src/redux/notices/noticesOperations.js
+++ src/redux/notices/noticesOperations.js
@@ -2,47 +2,49 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import {
   requestPublicNotices,
+  // requestRegisterNotices,
   removeNoticesById,
-  requestFavoriteNotices,
-  requestOwnNotices,
+  getRegisterNotices,
+  // requestOwnNotices,
   writeNewNotice,
   toggleFavorite,
 } from 'API/api';
 
 export const fetchNotices = createAsyncThunk(
   'fetchNotices',
-  async ({ categoryName, search }, { rejectWithValue, getState }) => {
-    const { token } = getState();
+  async ({ categoryName, search }, { rejectWithValue }) => {
+    try {
+      const notices = await requestPublicNotices(categoryName, search);
+      console.log('requestPublicNotices', notices);
+      return notices;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const fetchAuthNotices = createAsyncThunk(
+  'fetchAuthNotices',
+  async ({ token, categoryName, search = null }, { rejectWithValue }) => {
     console.log('tokenOper', token);
-    if (categoryName === 'favorite') {
-      try {
-        const notices = await requestFavoriteNotices(search);
-        console.log('notices favorite', notices);
-        return notices;
-      } catch (error) {
-        return rejectWithValue(error.message);
-      }
-    }
-
-    if (categoryName === 'own') {
-      try {
-        const notices = await requestOwnNotices(search, token);
-        console.log('notices own', notices);
-
-        return notices;
-      } catch (error) {
-        return rejectWithValue(error.message);
-      }
-    }
 
     try {
-      const notices = await requestPublicNotices({
-        category: categoryName,
-        search: search || null,
-      });
-      console.log('notices else', notices);
-
+      const notices = await getRegisterNotices(token, categoryName, search);
+      console.log('fetchAuthNotices', notices);
       return notices;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const setFavorite = createAsyncThunk(
+  'setFavorite',
+  async (data, { rejectWithValue }) => {
+    try {
+      const favorite = await toggleFavorite(data);
+      console.log('favorite Oper', favorite);
+      return favorite;
     } catch (error) {
       return rejectWithValue(error.message);
     }
@@ -74,15 +76,96 @@ export const removeNoticeFromUserById = createAsyncThunk(
   }
 );
 
-export const setFavorite = createAsyncThunk(
-  'setFavorite',
-  async (data, { rejectWithValue }) => {
-    try {
-      const favorite = await toggleFavorite(data);
-      console.log('favorite Oper', favorite);
-      return favorite;
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
+//     if (categoryName === 'own') {
+//       try {
+//         const notices = await requestOwnNotices(search, token);
+//         console.log('notices own', notices);
+
+//         return notices;
+//       } catch (error) {
+//         return rejectWithValue(error.message);
+//       }
+//     }
+
+//     if (token) {
+//       try {
+//         const notices = await requestRegisterNotices(
+//           categoryName,
+//           search,
+//           token
+//         );
+
+//         console.log('Register else', notices);
+//         return notices;
+//       } catch (error) {
+//         return rejectWithValue(error.message);
+//       }
+//     }
+
+//     try {
+//       const notices = await requestPublicNotices({
+//         category: categoryName,
+//         search: search || null,
+//       });
+//       console.log('notices else', notices);
+
+//       return notices;
+//     } catch (error) {
+//       return rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// export const fetchNotices = createAsyncThunk(
+//   'fetchNotices',
+//   async ({ categoryName, search }, token, { rejectWithValue }) => {
+//     console.log('tokenOper', token);
+//     if (categoryName === 'favorite') {
+//       try {
+//         const notices = await requestFavoriteNotices(search);
+//         console.log('notices favorite', notices);
+//         return notices;
+//       } catch (error) {
+//         return rejectWithValue(error.message);
+//       }
+//     }
+
+//     if (categoryName === 'own') {
+//       try {
+//         const notices = await requestOwnNotices(search, token);
+//         console.log('notices own', notices);
+
+//         return notices;
+//       } catch (error) {
+//         return rejectWithValue(error.message);
+//       }
+//     }
+
+//     if (token) {
+//       try {
+//         const notices = await requestRegisterNotices(
+//           categoryName,
+//           search,
+//           token
+//         );
+
+//         console.log('Register else', notices);
+//         return notices;
+//       } catch (error) {
+//         return rejectWithValue(error.message);
+//       }
+//     }
+
+//     try {
+//       const notices = await requestPublicNotices({
+//         category: categoryName,
+//         search: search || null,
+//       });
+//       console.log('notices else', notices);
+
+//       return notices;
+//     } catch (error) {
+//       return rejectWithValue(error.message);
+//     }
+//   }
+// );
