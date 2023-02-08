048acc7630fcc6fcde146c9f50b41fea141ae7a6
Yevhen Prokhorenko <97792710+YevhenProkhorenko@users.noreply.github.com>
2023-02-08 23:18:46 +0200

    fix header

--- src/components/Header/Header/Header.styled.js
+++ src/components/Header/Header/Header.styled.js
@@ -2,11 +2,10 @@ import { styled } from '@mui/material/styles';
 import { AppBar, Container, Box } from '@mui/material';
 
 export const Header = styled(AppBar)({
-  position: 'fixed',
   paddingTop: '12px',
   paddingBottom: '12px',
   color: `#111111`,
-  backgroundColor: `#FDF7F2`,
+  backgroundColor: `inherit`,
   boxShadow: 'none',
 });
 
