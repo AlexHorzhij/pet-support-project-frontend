f7e60a5c0ea8f6d08384537fbf7331b2e0066071
OleksandrHorzhij <horzhij@gmail.com>
2023-02-08 16:25:02 +0200

    pagination

--- src/components/NoticesPageComponents/NoticesGallery.js
+++ src/components/NoticesPageComponents/NoticesGallery.js
@@ -7,48 +7,76 @@ import { getAuth } from 'redux/auth/authSelectors';
 import { sortObjByDate } from 'services/sortObjByDate';
 import { removeNoticeFromUserById } from 'redux/notices/noticesOperations';
 
+import { useState } from 'react';
+import { Pagination } from '@mui/material';
+import usePagination from 'services/pagination';
+
 export default function NoticesGallery() {
-  const { items, 
+  const {
+    items,
     // error, isLoading
-   } = useSelector(getNotices);
+  } = useSelector(getNotices);
   const { token } = useSelector(getAuth);
   const dispatch = useDispatch();
   const data = sortObjByDate(items, 'create_at');
+
+  // ===========================
+  const [page, setPage] = useState(8);
+  const PER_PAGE = 8;
+
+  const count = Math.ceil(data.length / PER_PAGE);
+  const paginationData = usePagination(data, PER_PAGE);
+  const handleChangePagination = (event, page) => {
+    setPage(page);
+    paginationData.jump(page);
+  };
+  // =========================
 
   const deleteCard = e => {
     dispatch(removeNoticeFromUserById(e.target.id));
   };
 
-
   return (
-    <Grid container sx={{ pb: 6 }}>
-      {/* {error && <p>{error.data}</p>}
+    <>
+      <Grid container sx={{ pb: 6 }}>
+        {/* {error && <p>{error.data}</p>}
       <div style={{height: '30px', display: 'flex', justifyContent: 'center', width: '100%'}}>
         {isLoading ? <Loader /> : ''}
       </div> */}
-      {data &&
-        data.map(item => {
-          return (
-            <Grid
-              item
-              justifyContent="center"
-              display="flex"
-              xs={12}
-              sm={12}
-              md={6}
-              lg={3}
-              xl={3}
-              key={item._id}
-            >
-              <NoticesCardItem
-                token={token}
-                data={item}
-                deleteCard={deleteCard}
-              // openModal={openModal}
-              />
-            </Grid>
-          );
-        })}
-    </Grid>
+        {data &&
+          paginationData.currentData().map(item => {
+            return (
+              <Grid
+                item
+                justifyContent="center"
+                display="flex"
+                xs={12}
+                sm={12}
+                md={6}
+                lg={3}
+                xl={3}
+                key={item._id}
+              >
+                <NoticesCardItem
+                  token={token}
+                  data={item}
+                  deleteCard={deleteCard}
+                  // openModal={openModal}
+                />
+              </Grid>
+            );
+          })}
+      </Grid>
+      <Pagination
+        color="primary"
+        count={count}
+        size="large"
+        page={page}
+        variant="outlined"
+        shape="rounded"
+        onChange={handleChangePagination}
+        style={{ display: 'flex', justifyContent: 'center' }}
+      />
+    </>
   );
 }
