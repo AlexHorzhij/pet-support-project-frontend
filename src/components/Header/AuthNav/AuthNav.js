9d83e6533c3a29c8ee1f452ade469b57babdfcef
Yevhen Prokhorenko <97792710+YevhenProkhorenko@users.noreply.github.com>
2023-02-09 21:58:11 +0200

    style

--- src/components/Header/AuthNav/AuthNav.js
+++ src/components/Header/AuthNav/AuthNav.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import { AuthNavigation, AuthBtn, AuthLink } from './AuthNav.styled';
+import { AuthList, AuthItem, AuthLink } from './AuthNav.styled';
 
 export default function AuthNav() {
   return (
-    <AuthNavigation>
-      <AuthBtn variant="outlined">
+    <AuthList>
+      <AuthItem>
         <AuthLink to="/login">Login</AuthLink>
-      </AuthBtn>
-      <AuthBtn variant="outlined">
+      </AuthItem>
+      <AuthItem>
         <AuthLink to="register">Registration</AuthLink>
-      </AuthBtn>
-    </AuthNavigation>
+      </AuthItem>
+    </AuthList>
   );
 }
--- src/components/Header/AuthNav/AuthNav.styled.js
+++ src/components/Header/AuthNav/AuthNav.styled.js
@@ -11,6 +11,44 @@ export const AuthNavigation = styled(Box)({
     marginLeft: '246px',
   },
 });
+export const AuthList = styled.ul`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+  @media screen and (max-width: 767px) {
+    display: none;
+  }
+  @media screen and (min-width: 768px) {
+    margin-left: 246px;
+  }
+`;
+
+export const AuthItem = styled.li`
+  font-family: 'Manrope';
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 1.35;
+  align-items: center;
+  letter-spacing: 0.04em;
+  text-decoration: none;
+  &:not(:last-child) {
+    margin-right: 12px;
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
+  @media screen and (min-width: 1280px) {
+    &:not(:last-child) {
+      margin-right: 20px;
+    }
+  }
+  &:first-child.active {
+    border-radius: 40px;
+    color: #ffffff;
+    background: #f59256;
+  }
+`;
 
 export const AuthBtn = styled(Button)`
   @media screen and (min-width: 768px) {
@@ -23,18 +61,41 @@ export const AuthBtn = styled(Button)`
       margin-right: 20px;
     }
   }
+  &.active {
+    color: #ffffff;
+    background: #f59256;
+  }
 `;
 export const AuthLink = styled(NavLink)`
-  text-transform: capitalize;
-  color: inherit;
-  @media screen and (min-width: 768px) {
-    font-family: 'Manrope';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 27px;
-    align-items: center;
-    letter-spacing: 0.04em;
-  }
-  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 10px 28px;  
+  align-items: center;
+  pointer-events: initial;
+  height: 44px;
+  color: #111111;
+  background: #ffffff;
+  border: 2px solid #f59256;
+  border-radius: 40px;
+ text-decoration: none; 
+  &.active {
+  color: #ffffff;
+  background: #f59256;
 `;
+
+// export const AuthLink = styled(NavLink)`
+//   text-transform: capitalize;
+//   color: inherit;
+//   @media screen and (min-width: 768px) {
+//     font-family: 'Manrope';
+//     font-style: normal;
+//     font-weight: 500;
+//     font-size: 20px;
+//     line-height: 27px;
+//     align-items: center;
+//     letter-spacing: 0.04em;
+//   }
+//   text-decoration: none;
+// `;
