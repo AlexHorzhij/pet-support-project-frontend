38ba623a57c1f7584094e213049a73502e5c87da
Oleksandr Tolochko <0504Fusion@gmail.com>
2023-02-09 10:41:12 +0200

    Update newsSearch.styled.js

--- src/components/News/NewsSearch/newsSearch.styled.js
+++ src/components/News/NewsSearch/newsSearch.styled.js
@@ -2,6 +2,9 @@ import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
 export const FormSearch = styled(Paper)({
+  display: 'flex',
+  alignItems: 'center',
+  maxWidth: '608px',
   background: '#FFFFFF',
   borderRadius: '40px',
   boxShadow: '7px 4px 14px rgba(49, 21, 4, 0.07)',
@@ -9,4 +12,5 @@ export const FormSearch = styled(Paper)({
   marginLeft: 'auto',
   marginTop: '40px',
   marginBottom: '60px',
+  padding: '2px 4px',
 });
