d784820730d6511d633c90539198f8e4037ac5de
Peter <peteryurich@gmail.com>
2023-02-11 14:58:45 +0200

    scroll-up-btn

--- src/pages/NoticePage.js
+++ src/pages/NoticePage.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { Container } from '@mui/material';
-// import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { IconButton } from '@mui/material';
 import { useTheme } from '@mui/material';
@@ -55,11 +53,11 @@ export default function NoticePage() {
               width: '60px', height: '60px', color: 'lightgray',
               position: 'fixed', right: 100, bottom: 80, zIndex: 1,
               boxShadow: `inset 0 0 0 5px ${palette.primary.main}`,
-              transition: '0.3s', transform: 'translateY(150px)',
+              transition: '0.4s',
+              transform: offset > 100 ? 'translateY(0)' : 'translateY(150px)',
               '&:hover': {
                 color: 'primary.main'
               },
-              transform: offset > 100 ? 'translateY(0)' : 'translateY(150px)',
             }
             } />
         </IconButton>}
