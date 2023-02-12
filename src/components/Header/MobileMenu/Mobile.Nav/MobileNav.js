4601d824c48b5d5d23ad8b684b051889bc8e5fd9
Yevhen Prokhorenko <97792710+YevhenProkhorenko@users.noreply.github.com>
2023-02-12 20:28:16 +0200

    fix

--- src/components/Header/MobileMenu/Mobile.Nav/MobileNav.js
+++ src/components/Header/MobileMenu/Mobile.Nav/MobileNav.js
@@ -8,26 +8,25 @@ import {
 import { useMenu } from 'components/Header/Context/menuContext';
 import { useTranslation } from 'react-i18next';
 
-
 export default function MobileNav() {
-  const {t} = useTranslation('common')  
+  const { t } = useTranslation('common');
   const { close } = useMenu();
   return (
     <MobileNavigation>
       <MobileNavList>
         <MobileNavListItem>
           <MobileNavLink to="/news" onClick={close}>
-            {t('MobileMenu.1link')} 
+            {t('MobileMenu.1link')}
           </MobileNavLink>
         </MobileNavListItem>
         <MobileNavListItem>
           <MobileNavLink to="/notices/sell" onClick={close}>
-            {t('MobileMenu.2link') }
+            {t('MobileMenu.2link')}
           </MobileNavLink>
         </MobileNavListItem>
         <MobileNavListItem>
           <MobileNavLink to="/friends" onClick={close}>
-            {t('MobileMenu.3link') }
+            {t('MobileMenu.3link')}
           </MobileNavLink>
         </MobileNavListItem>
       </MobileNavList>
