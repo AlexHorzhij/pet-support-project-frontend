788a33fb226fb286abc9776bfa62841916ecf73f
Yevhen Prokhorenko <97792710+YevhenProkhorenko@users.noreply.github.com>
2023-02-13 00:09:21 +0200

    Turn to Navlink

--- src/components/Header/Logo/Logo.styled.js
+++ src/components/Header/Logo/Logo.styled.js
@@ -1,21 +1,22 @@
 import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
 
 export const LogoWrapper = styled.div`
   font-weight: 700;
 `;
-export const LogoInfo = styled.a`
-  font-family: Poppins;
-  font-weight: 700;
-  font-size: 28px;
-  line-height: 1.7;
-  letter-spacing: 0.07em;
-  text-decoration: none;
-  color: inherit;
-  @media screen and (min-width: 768px) {
-    font-size: 32px;
-    line-height: 1.5;
-  }
-`;
+export const LogoInfo = styled(NavLink)({
+  fontFamily: 'Poppins',
+  fontWeight: '700',
+  fontSize: '28px',
+  lineHeight: '1.7',
+  letterSpacing: '0.07em',
+  textDecoration: 'none',
+  color: 'inherit',
+  '@media screen and (min-width: 768px)': {
+    fontSize: '32px',
+    lineHeight: '1.5',
+  },
+});
 
 export const Span = styled.span`
   color: orange;
